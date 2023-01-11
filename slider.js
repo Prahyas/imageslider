d16c788f65ac54f408152e2618268002c46655ad
prahyas <xrestha17@gmail.com>
2023-01-10 20:09:08 -0600

    image slider completed

--- slider.js
+++ slider.js
@@ -2,8 +2,34 @@ const leftArrow = document.querySelector('.leftarrow');
 const rightArrow = document.querySelector('.rightarrow');
 const slider = document.querySelector('.slider');
 const images = document.querySelectorAll('.image');
+const bottom = document.querySelector('.bottom');
 
 let slideNumber = 1;
+
+for (i = 0; i < images.length; i++) {
+  const div = document.createElement('div');
+  div.className = 'button';
+  bottom.appendChild(div);
+}
+
+const buttons = document.querySelectorAll('.button');
+
+buttons[0].style.backgroundColor = 'white';
+
+const resetButtonBg = () => {
+  buttons.forEach((button, i) => {
+    button.style.backgroundColor = 'transparent';
+  });
+};
+
+buttons.forEach((button, i) => {
+  button.addEventListener('click', () => {
+    slider.style.transform = `translateX(-${i * 450}px)`;
+    resetButtonBg();
+    button.style.backgroundColor = 'white';
+    slideNumber = i + 1;
+  });
+});
 
 leftArrow.addEventListener('click', () => {
   if (slideNumber > 1) {
@@ -13,6 +39,8 @@ leftArrow.addEventListener('click', () => {
     slider.style.transform = `translateX(-${(images.length - 1) * 450}px)`;
     slideNumber = images.length;
   }
+  resetButtonBg();
+  buttons[slideNumber - 1].style.backgroundColor = 'white';
   console.log('slideNumber', slideNumber);
 });
 
@@ -24,5 +52,7 @@ rightArrow.addEventListener('click', () => {
     slider.style.transform = `translateX(0px)`;
     slideNumber = 1;
   }
+  resetButtonBg();
+  buttons[slideNumber - 1].style.backgroundColor = 'white';
   console.log('slideNumber', slideNumber);
 });
